22205d135da676147da84419a901fe8b2cb945bc
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-08-06 09:32:25 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,46 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SidBar from './SidBar'
-import Home from './Home'
-import { useEffect, useState } from 'react'
-import User from './pages/user'
-
-
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [myData, setMyData] = useState([]);
 
-  const [users, setUsers] = useState([]);
-  useEffect(() =>{
-    fetchData();
-  }, [])
-
-  const fetchData = async () => {
-    await fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then((data) => setUsers(data))
-    .catch((err) => {
-      console.log(err);
-    })
-  }
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => setMyData(res.data))
+      .catch((error) => {console.log(error);})
+      .finally(() => {
+        
+      })
+    }, []);
 
   return (
     <>
-      <BrowserRouter>
-        <main className='flex '>
-          <div className='px-[12px] flex flex-col gap-[6px] py-[12px] bg-[#f7f7f7] h-[100vh] overflow-auto w-[260px] '>
-            {
-              users.map((user) => (
-                <SidBar user = {user.name} />
-              ))
-            }
+      <div className='max-w-[900px] mx-auto py-[32px]'>
+      {myData.map((post) => {
+        return (
+          <div className='mt-[22px] border rounded-[5px] p-[22px] '>
+            <h2 className='text-[15px] text-center '>{post.title}</h2>
+            <p className='text-[12px] text-center mt-[7px] '>{post.body}</p>
           </div>
-          <div className='w-full overflow-auto h-[100vh] '>
-            <Routes>
-              <Route path='/user' element = {<User/>} />
-              <Route path='/' element = {<Home/>} />
-              <Route path='*' element = {'Wrong Page'}/>
-            </Routes>
-          </div>
-        </main>
-      </BrowserRouter>
+        );
+      })}
+    </div>
+    <section className='h-[100vh] w-full flex items-center justify-center bg-[#f1f0ee]'>
+      <div className='flex rounded-[12px] overflow-hidden max-w-[900px]'>
+        <div className='flex-1'>
+
+        </div>
+        <div
+          className='bg-cover bg-no-repeat bg-center h-[500px] flex-1'
+          style={{ backgroundImage: 'url(https://ayazakramawan.com/wp-content/uploads/2022/12/Untitled-design-46.png)' }}>
+
+        </div>
+      </div>
+    </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
